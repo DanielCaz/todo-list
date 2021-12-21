baf35b42452a0ff33f626f44f64378cadb4aade3
DanielCaz <cazarez.jorge.prog2019@gmail.com>
2021-12-21 13:27:08 -0600

    Added firebase

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,6 @@
+import { getAuth, signOut } from "firebase/auth";
+import { useRouter } from "next/router";
+import app from "../firebase";
 import styles from "../styles/Header.module.scss";
 
 const Header = ({
@@ -7,11 +10,25 @@ const Header = ({
   title: string;
   userStatus: number;
 }) => {
+  const router = useRouter();
+  const auth = getAuth(app);
+
+  const onButtonClick = () => {
+    if (userStatus === 0) {
+      router.replace("/");
+    } else if (userStatus === 1) {
+      router.replace("/");
+      signOut(auth);
+    }
+  };
+
   return (
     <header className={styles.header}>
       <h1>{title}</h1>
       {userStatus > -1 && (
-        <button>{userStatus === 0 ? "Sign In" : "Sign Out"}</button>
+        <button onClick={onButtonClick}>
+          {userStatus === 0 ? "Sign In" : "Sign Out"}
+        </button>
       )}
     </header>
   );
--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   reactStrictMode: true,
-}
+  env: {
+    API_KEY: "AIzaSyDDzjFrRCaqayBT4WJZ0LPRjeoG5yBeuf4",
+    AUTH_DOMAIN: "todo-app-1f2d2.firebaseapp.com",
+    PROJECT_ID: "todo-app-1f2d2",
+    STORAGE_BUCKET: "todo-app-1f2d2.appspot.com",
+    MESSAGING_SENDER_ID: "833425613437",
+    APP_ID: "1:833425613437:web:8caebad9dc6bb96c930714",
+    MEASUREMENT_ID: "G-Z8Z3GRKWEH",
+  },
+};
